b2221f87efddaeebc36e650b88af78fba7d42843
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-24 23:50:11 -0300

    JSON messages

--- src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
+++ src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
@@ -1,13 +1,16 @@
 package br.edu.ifgoiano.shippingcompany.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.edu.ifgoiano.shippingcompany.service.RabbitMQService;
 import constants.RabbitMQConstants;
 import dto.ProductDto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class ProductConsumer {
@@ -16,9 +19,12 @@ public class ProductConsumer {
 	private RabbitMQService rabbitMQService;
 
 	@RabbitListener(queues = RabbitMQConstants.QUEUE_SEND_PRODUCTS)
-	private void consumer(ProductDto productDto) {
+	private void consumer(String message) throws JsonMappingException, JsonProcessingException {
+		ProductDto productDto = new ObjectMapper().readValue(message, ProductDto.class);
+		
 		System.out.println(productDto.id + ", " + productDto.name + ", " + productDto.sender + ", "
 				+ productDto.receiver + ", " + productDto.weight);
+		
 		if (productDto.weight > 10.0)
 			this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_HEAVY_PRODUCTS, productDto);
 		else
