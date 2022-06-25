bcf44c710994f76f7eb560d677fb4503df0e60c8
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-25 14:29:47 -0300

    Sending JSON messages

--- src/main/java/br/edu/ifgoiano/shippingcompany/service/RabbitMQService.java
+++ src/main/java/br/edu/ifgoiano/shippingcompany/service/RabbitMQService.java
@@ -4,14 +4,21 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class RabbitMQService {
 
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
-	
+		
 	public void sendMessage(String queueName, Object message) {
-		this.rabbitTemplate.convertAndSend(queueName, message);
+		try {
+			String jsonMessage = new ObjectMapper().writeValueAsString(message);	
+			this.rabbitTemplate.convertAndSend(queueName, jsonMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
