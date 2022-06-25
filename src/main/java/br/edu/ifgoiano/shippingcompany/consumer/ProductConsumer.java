c66f4c7184a6f5174441c3ce039bdf6e9ff70283
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-25 15:27:28 -0300

    Consuming RESPONSE queue

--- src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
+++ src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import br.edu.ifgoiano.shippingcompany.service.RabbitMQService;
 import constants.RabbitMQConstants;
 import dto.ProductDto;
+import dto.ReportDto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -19,7 +20,7 @@ public class ProductConsumer {
 	private RabbitMQService rabbitMQService;
 
 	@RabbitListener(queues = RabbitMQConstants.QUEUE_SEND_PRODUCTS)
-	private void consumer(String message) throws JsonMappingException, JsonProcessingException {
+	private void consumeSendProducts(String message) throws JsonMappingException, JsonProcessingException {
 		ProductDto productDto = new ObjectMapper().readValue(message, ProductDto.class);
 		
 		System.out.println(productDto.id + ", " + productDto.name + ", " + productDto.sender + ", "
@@ -29,6 +30,15 @@ public class ProductConsumer {
 			this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_HEAVY_PRODUCTS, productDto);
 		else
 			this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_LIGHT_PRODUCST, productDto);
+	}
+	
+	@RabbitListener(queues = RabbitMQConstants.QUEUE_RESPONSE)
+	private void consumeResponse(String message) throws JsonMappingException, JsonProcessingException {
+		ReportDto reportDto = new ObjectMapper().readValue(message, ReportDto.class);
+		
+		System.out.println(reportDto.id + ", " + reportDto.productId + ", " + reportDto.enterprise + ", " + reportDto.deliveryDate);
+		
+		this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_REPORT, reportDto);
 	}
 
 }
