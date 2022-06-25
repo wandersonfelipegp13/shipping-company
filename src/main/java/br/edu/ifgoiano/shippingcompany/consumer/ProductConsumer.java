06c76ff23afdd03ee4285ce14914239a2d8bec1f
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-24 23:26:42 -0300

    Sending to queue based on weight

--- src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
+++ src/main/java/br/edu/ifgoiano/shippingcompany/consumer/ProductConsumer.java
@@ -1,18 +1,28 @@
 package br.edu.ifgoiano.shippingcompany.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.edu.ifgoiano.shippingcompany.service.RabbitMQService;
 import constants.RabbitMQConstants;
 import dto.ProductDto;
 
 @Component
 public class ProductConsumer {
 
+	@Autowired
+	private RabbitMQService rabbitMQService;
+
 	@RabbitListener(queues = RabbitMQConstants.QUEUE_SEND_PRODUCTS)
 	private void consumer(ProductDto productDto) {
 		System.out.println(productDto.id + ", " + productDto.name + ", " + productDto.sender + ", "
 				+ productDto.receiver + ", " + productDto.weight);
+		if (productDto.weight > 10.0)
+			this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_HEAVY_PRODUCTS, productDto);
+		else
+			this.rabbitMQService.sendMessage(RabbitMQConstants.QUEUE_LIGHT_PRODUCST, productDto);
 	}
 
 }
